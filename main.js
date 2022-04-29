9dc019ec0cafb2015c7e07d3cb4b1fb98e7fa05a
Ikilzig <palvarezimaz@gmail.com>
2022-04-29 16:45:44 +1000

    working on players turns

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-// console.log('hola');
-
-// var playerPlay = event.target;
-// var playBoard = document.querySelector('board');
-// console.log(board);
-
-// var boardGame = document.querySelector('board');
-// boardGame.addEventListener('click');
-
-// // var clickedBox = event.target;
+//
+var generalTurnCounter = 1;
 
 // // function for writing inside the divs -- its only using player 1
 function player1(event) {
-  var squareClickedOn = event.target;
+  // var squareClickedOn = this.event;
   if (squareClickedOn.tagName === 'DIV') {
     if (
       !squareClickedOn.classList.contains('markingsplayer1') &&
@@ -19,21 +11,43 @@ function player1(event) {
     )
       squareClickedOn.classList.add('markingsplayer1');
   }
+  if (generalTurnCounter % 2 !== 0) {
+    console.log('player1');
+  }
+  console.log(generalTurnCounter);
 }
 
 function player2(event) {
-  var squareClickedOn = event.target;
+  // var squareClickedOn = this.target;
   if (squareClickedOn.tagName === 'DIV') {
     if (
       !squareClickedOn.classList.contains('markingsplayer1') &&
       !squareClickedOn.classList.contains('markingsplayer2')
     ) {
-      console.log('1');
       squareClickedOn.classList.add('markingsplayer2');
     }
   }
 }
+// Step 3 - players (functions) take turns to play
+var generalTurnCounter = 1;
+function turnCount() {
+  generalTurnCounter++;
+}
 
-document.querySelector('.board').addEventListener('click', player1);
+function playersTurn(event) {
+  var squareClickedOn = event.target;
+  generalTurnCounter++;
+  if (generalTurnCounter % 2 !== 0) {
+    player1();
+  } else {
+    player2();
+  }
+}
 
-document.querySelector('.board').addEventListener('contextmenu', player2);
+document.querySelector('.board').addEventListener('click', playersTurn);
+
+// Functions to test if step 2 and 2.1 work
+// document.querySelector('.board').addEventListener('click', player1);
+
+// document.querySelector('.board').addEventListener('contextmenu', player2);
+// console.log(player2);
