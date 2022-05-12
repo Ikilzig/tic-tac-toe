a8a274b0d7c1d042f3dae056f18a0160b8045398
Ikilzig <palvarezimaz@gmail.com>
2022-05-13 08:55:04 +1000

    working on responsive design

---
+++ backup/back up pre entrega/README.md
@@ -0,0 +1,117 @@
+# TIC - TAC - TOE
+
+## Summary
+
+This is a take on the classic TIC-TAC-TOE that pays homage to one of the best video games EVER (at least of my childhood...), called Monkey Island.
+
+It was built as the 1st Project (3rd week) of the General Assembly Software Engineer Immersive, 12-week course, in May 2022.
+
+[Click here to play the game.](https://ikilzig.github.io/tic-tac-toe/)
+
+(The game is optmized for a minimum width of 1024px)
+
+## Conclusion and thoughts.
+
+After three weeks of the SEI course, I had enough working experience with JS to be comfortable with coding the general game dynamics. I stump against a few problems, but I could solve them quite quickly, as I think I did solid planning of the basics. I had a working game after an afternoon playing with it (Friday: steps 2 and 3). I then used half a day to work on the CSS (Saturday: step 4). I must admit that CSS was quite challenging and I still don't think I got the best design. It works and it looks good, but it still has room for improvement (redesign). Step 5 and onwards were implemented half Sunday and most of Monday. Something that I noticed is that the deeper and more specific the functionality, the longer it took me to work it out.
+There were plenty of options for add-ons, but I chose the ones that I found most difficult, this being creating a more responsive CSS and widely implementing localstorage. These two are the main shortcomings of this project, considering the available allocated time.
+Overall, it was great to have a take on a classic meeting a classic. It's a project I've enjoyed plenty as it brought me so many good memories while settling all these new programming concepts.
+
+## Plan for solving the problem
+
+### Plan
+
+Considering that these are my first steps, the general approach would be first to make it work. Then I will try to make it look nice and, lastly, make it better.
+
+#### Step 1
+
+1. I drew a couple of design sheets, one with a general approach:
+   ![general plan](./images/general-plan.jpg)
+   And a second one that details the flow of the program and the subroutines:
+   ![flow and subroutines](./images/sub-routines.jpg)
+
+2. I wrote this README.md file, which is also a great opportunity to plan the next steps.
+
+#### Step 2 - HTML wireframe.
+
+1. I began with the HTML wireframe shown in the first image.
+2. Once the HTML was roughly outlined, I did some basic CSS to give it a rough visual layout.
+3. I added some pre-emptive classes and IDs for the next step.
+
+#### Step 3 - JS - Make it work
+
+1. From this step onwards, I will start coding the behaviour of the site mainly JS.
+
+I used the same principle over and over again:
+
+### Build simple, brick by brick.
+
+==This readme file will be updated along the project==
+
+- ~~Draw the grid: a 3x3 array~~
+- ~~Draw something on the grid~~
+- ~~Be sure that the drawings don't overlap and you can only draw on empty spots~~
+- ~~Implement 2 players taking turns (idea to use: a counter, odd player1, even player2, image sub-routines)~~
+- ~~These 2 players draw something different~~
+- ~~Implement the flow of the program (image sub-routines)~~
+- ~~Implement the rules for winning. Use array positions and a brute-force approach~~
+
+### Step 4 - CSS and visuals - make it look nice
+
+Now that the prototype works, I've started polishing the visual approach. There are several things to consider here. The main one was to choose a theme, which I already have: the first Monkey Island video play. Thus, the next things to complete are:
+
+- ~~Find the two main characters and the game logo and edit the images to fit the page~~
+- ~~Layout a nice board and different chips for each player~~
+- ~~Make them look good and harmonious~~
+- ~~Edit all the other standing elements to give a better game immersion~~
+- ~~Change font style~~
+
+#### JS
+
+- ~~Convert all the alerts to on-screen messages. Use a tag and textContent~~
+- ~~Add a reset button~~
+- ~~Add a total reset button~~
+- ~~Add messages stating whose turn is next~~
+
+### Step 5 - Add extra functionalities - Make it better
+
+- ~~Add audio effects on each play (one for swords and one for bombs)~
+- ~~Add interaction between the players -classic Monkey Island insults). The arrays are on the js already~~
+- ~~Add an initial screen, as overlay with a transition~~
+- ~~intro and winning music~~(source https://scummbar.com/resources/downloads/index.php?todo=MP3) -~~LocalStorage for points~~
+
+==Things to work out==
+
+- Make the audio on/off option persistent beyond a page refrech
+- Make the initial screen appear only when the user restarts the whole game and not on every refresh
+- Add more game characters and allow player selection
+- MAKE IT RESPONSIVE!
+
+## Tech used
+
+- Amazing search engines
+- GitHub
+- HTML
+- CSS
+- JavaScript
+- Image editors
+- Adobe Color wheel
+
+Cool things (from a beginner point of view) used:
+
+- localStorage
+- CSS effects
+- Audio
+
+## Lessons learned
+
+- Coding is hard
+- Coding is also fun (even if the code is still not working)
+- Parvus error in principio, magnus est in fine (A small error at the beginning [planning] is a big error at the end of the job)
+- There are plenty of free images and image kits out there
+- You don't need to be a genius to use an online image editor
+- But you always need to check a color wheel like Adobe
+- It's hard to work with colors and contrasts.
+- CSS is very flexible and it DOES have a logic behind it
+- ==That there is a balance between it works and make it better, and between adding new functionalities and redesigning the whole thing to fit them.==
+
+<a href="https://ikilzig.github.io/tic-tac-toe/" target="_blank">Click here to play the game</a>
---
+++ backup/back up pre entrega/main.js
@@ -0,0 +1,399 @@
+////////////// STARTING CONDITIONS - UNDER DEVELOPMENT
+// function start() {
+//   if (localStorage.getItem('startScreenCondition') == '1') {
+//     document.querySelector('.initial-hello').classList.remove('fade-out-image');
+//   }
+//   if (audioMute[i].muted) {
+//     audioMute[i].muted = false;
+//     document.querySelector('.mute').textContent = 'Audio On';
+//   } else {
+//     audioMute[i].muted = true;
+//     document.querySelector('.mute').textContent = 'Audio Off';
+//   }
+// }
+
+// start();
+
+// general counters
+var playerOneWinCounter = localStorage.getItem('persistentScorePlayerOne');
+var playerTwoWinCounter = localStorage.getItem('persistentScorePlayerTwo');
+var generalTurnCounter = 0;
+
+// general variables
+var boardGameArray = document.querySelectorAll('div');
+console.log(boardGameArray);
+
+// Starting conditions
+speechBubblePlayerOneDisappar();
+speechBubblePlayerTwoDisappar();
+
+if (playerOneWinCounter !== 0) {
+  document.querySelector('.points-player-one').textContent =
+    playerOneWinCounter;
+} else {
+  document.querySelector('.points-player-one').textContent = '0';
+}
+
+if (playerTwoWinCounter !== 0) {
+  document.querySelector('.points-player-two').textContent =
+    playerTwoWinCounter;
+} else {
+  document.querySelector('.points-player-two').textContent = '0';
+}
+
+////////////// BUTTONS ///////////
+/// GAME START
+document.querySelector('.start-the-game').addEventListener('click', gameStart);
+
+function gameStart() {
+  localStorage.setItem('startScreenCondition', 'off');
+  document.querySelector('.initial-hello').classList.add('fade-out-image');
+  document.querySelector('.intro-audio').play();
+}
+
+//Start message if false, remove class that removes it - NOT WORKING
+function start() {
+  if (localStorage.getItem('startScreenCondition') === 'on') {
+    document.querySelector('.initial-hello').classList.remove('fade-out-image');
+  }
+}
+
+// button play again
+document
+  .querySelector('.reset-button')
+  .addEventListener('click', resetBoardForNextGame);
+
+function resetBoardForNextGame(event) {
+  for (i = 0; i < boardGameArray.length; i++) {
+    boardGameArray[i].classList.remove('markings-player-one');
+    boardGameArray[i].classList.remove('markings-player-two');
+  }
+  generalTurnCounter = 0;
+  document.querySelector('.message-board').textContent = "Let's do it, Pirate!";
+  document.querySelector('.player-one-name').classList.remove('winner');
+  document.querySelector('.player-two-name').classList.remove('winner');
+  speechBubblePlayerOneDisappar();
+  speechBubblePlayerTwoDisappar();
+  document.querySelector('.dialogs-player-one').textContent = ' ';
+  document.querySelector('.dialogs-player-two').textContent = ' ';
+}
+
+//reset all the game button
+document
+  .querySelector('.reset-all-game')
+  .addEventListener('click', resetAllScore);
+
+function resetAllScore() {
+  document.querySelector('.points-player-one').textContent = 0;
+  document.querySelector('.points-player-two').textContent = 0;
+  document.querySelector('.message-board').textContent =
+    'Argg, that damn Monkey Curse!';
+  resetBoardForNextGame();
+  speechBubblePlayerOneDisappar();
+  speechBubblePlayerTwoDisappar();
+  document.querySelector('.dialogs-player-one').textContent = ' ';
+  document.querySelector('.dialogs-player-two').textContent = ' ';
+  localStorage.setItem('persistentScorePlayerTwo', 0);
+  localStorage.setItem('persistentScorePlayerOne', 0);
+  playerOneWinCounter = 0;
+  playerTwoWinCounter = 0;
+  localStorage.setItem('startScreenCondition', 'off');
+  document.querySelector('.initial-hello').classList.remove('fade-out-image');
+}
+// MUTE AUDIO ///
+var audioMute = document.querySelectorAll('#audio');
+function muteAudio() {
+  for (i = 0; i < audioMute.length; i++) {
+    if (audioMute[i].muted) {
+      audioMute[i].muted = false;
+      document.querySelector('.mute').textContent = 'Audio On';
+    } else {
+      audioMute[i].muted = true;
+      document.querySelector('.mute').textContent = 'Audio Off';
+    }
+  }
+}
+
+document.querySelector('.mute').addEventListener('click', muteAudio, false);
+
+// ABOUT ME
+
+function aboutMePopup() {
+  var aboutMe = document.getElementById('aboutmetext');
+  aboutMe.classList.toggle('show');
+}
+
+document
+  .querySelector('.information-pop-up')
+  .addEventListener('click', aboutMePopup);
+
+///////////////// Pirate insults //////////
+var playerOneInsultsArray = [
+  'This is the end, you gutter-crawling cur!',
+  "Soon you'll be wearing my sword like a kebab!",
+  'My handkerchief will wipe up your blood!',
+  'I once owned a dog that was smarter then you',
+  'You make me want to puke',
+  "Nobody's ever drawn blood from me",
+  'You fight like a dairy farmer',
+  'I got this scar during a mighty struggle!',
+  "I've heard you were a contemptible sneak",
+  'Have you stopped wearing diapers yet?',
+  'There are no clever moves that can help you now',
+  'Now I know what filth and stupidity really are',
+];
+
+var playerTwoInsultsArray = [
+  "You're no match for my brains, you poor fool",
+  'You have the manners of a beggar',
+  "I'm not going to take your insolence like that!",
+  'There are no words for how disgusting you are',
+  "I've spoken with apes more polite then you",
+  'My wisest enemies run away from me!',
+  'Only once have I met such a coward!',
+  'I hope you have a boat ready for a quick escape',
+  'Every word you say to me is stupid',
+  'You are a pain in the backside, sir!',
+  'I see people like you passed-out on the local pub',
+];
+
+///////////// Game Flow //////////
+// Function that regulates players turns
+function playersTurn(event) {
+  // This runs up to 9 times
+  if (generalTurnCounter < boardGameArray.length) {
+    var squareClickedOn = event.target;
+    if (generalTurnCounter % 2 == 0) {
+      if (squareClickedOn.tagName === 'DIV') {
+        if (
+          !squareClickedOn.classList.contains('markings-player-one') &&
+          !squareClickedOn.classList.contains('markings-player-two')
+        )
+          squareClickedOn.classList.add('markings-player-one');
+        document.querySelector('.swords-audio').play();
+        generalTurnCounter++;
+        document.querySelector('.message-board').textContent =
+          "It is now Capitan LeChuck's turn, he throws bombs";
+        playerOneDialogEachTurn();
+      }
+      RulesForWinning();
+
+      // console.log(generalTurnCounter); // checkpoint
+    } else {
+      if (squareClickedOn.tagName === 'DIV') {
+        if (
+          !squareClickedOn.classList.contains('markings-player-one') &&
+          !squareClickedOn.classList.contains('markings-player-two')
+        ) {
+          squareClickedOn.classList.add('markings-player-two');
+          document.querySelector('.bomb-audio').play();
+          generalTurnCounter++;
+          document.querySelector('.message-board').textContent =
+            "It is now Guybrush Treepwood's turn, he fights with swords";
+          playerTwoDialogEachTurn();
+        }
+      }
+      RulesForWinning();
+      // console.log(generalTurnCounter); // checkpoint
+    }
+  } else {
+    alert('The game is over! Click on Play again to.. play again');
+  }
+}
+// players playing
+document.querySelector('.board').addEventListener('click', playersTurn);
+
+//winningfunctions
+
+function playerOneWins() {
+  playerOneWinCounter++;
+  localStorage.setItem('persistentScorePlayerOne', playerOneWinCounter);
+  generalTurnCounter = generalTurnCounter + boardGameArray.length;
+  document.querySelector('.points-player-one').textContent =
+    playerOneWinCounter;
+  document.querySelector('.message-board').textContent =
+    'Guybrush Treepwood wins!';
+  document.querySelector('.win-audio').play();
+  document.querySelector('.player-one-name').classList.add('winner');
+  speechBubblePlayerOneDisappar();
+  document.querySelector('.dialogs-player-one').textContent = ' ';
+}
+
+function playerTwoWins() {
+  playerTwoWinCounter++;
+  localStorage.setItem('persistentScorePlayerTwo', playerTwoWinCounter);
+  generalTurnCounter = generalTurnCounter + boardGameArray.length;
+  document.querySelector('.points-player-two').textContent =
+    playerTwoWinCounter;
+  document.querySelector('.message-board').textContent =
+    'Captain LeChuck wins!';
+  document.querySelector('.win-audio').play();
+  document.querySelector('.player-two-name').classList.add('winner');
+  speechBubblePlayerTwoDisappar();
+  document.querySelector('.dialogs-player-two').textContent = ' ';
+}
+
+//rules for winning player 1
+// conditions for winning row 1
+function RulesForWinning() {
+  if (
+    boardGameArray[0].classList.contains('markings-player-one') &&
+    boardGameArray[1].classList.contains('markings-player-one') &&
+    boardGameArray[2].classList.contains('markings-player-one')
+  ) {
+    playerOneWins();
+  } else if (
+    //row 2
+    boardGameArray[3].classList.contains('markings-player-one') &&
+    boardGameArray[4].classList.contains('markings-player-one') &&
+    boardGameArray[5].classList.contains('markings-player-one')
+  ) {
+    playerOneWins();
+  } else if (
+    // row 3
+    boardGameArray[6].classList.contains('markings-player-one') &&
+    boardGameArray[7].classList.contains('markings-player-one') &&
+    boardGameArray[8].classList.contains('markings-player-one')
+  ) {
+    playerOneWins();
+  } else if (
+    //column 1
+    boardGameArray[0].classList.contains('markings-player-one') &&
+    boardGameArray[3].classList.contains('markings-player-one') &&
+    boardGameArray[6].classList.contains('markings-player-one')
+  ) {
+    playerOneWins();
+  } else if (
+    //column 2)
+    boardGameArray[1].classList.contains('markings-player-one') &&
+    boardGameArray[4].classList.contains('markings-player-one') &&
+    boardGameArray[7].classList.contains('markings-player-one')
+  ) {
+    playerOneWins();
+  } else if (
+    // column 3)
+    boardGameArray[2].classList.contains('markings-player-one') &&
+    boardGameArray[5].classList.contains('markings-player-one') &&
+    boardGameArray[8].classList.contains('markings-player-one')
+  ) {
+    playerOneWins();
+  } else if (
+    //diagonal 1
+    boardGameArray[0].classList.contains('markings-player-one') &&
+    boardGameArray[4].classList.contains('markings-player-one') &&
+    boardGameArray[8].classList.contains('markings-player-one')
+  ) {
+    playerOneWins();
+  } else if (
+    // diagonal 2
+    boardGameArray[2].classList.contains('markings-player-one') &&
+    boardGameArray[4].classList.contains('markings-player-one') &&
+    boardGameArray[6].classList.contains('markings-player-one')
+  ) {
+    playerOneWins();
+  } else if (
+    // Player 2
+    //row 1
+    boardGameArray[0].classList.contains('markings-player-two') &&
+    boardGameArray[1].classList.contains('markings-player-two') &&
+    boardGameArray[2].classList.contains('markings-player-two')
+  ) {
+    playerTwoWins();
+  } else if (
+    //row 2
+    boardGameArray[3].classList.contains('markings-player-two') &&
+    boardGameArray[4].classList.contains('markings-player-two') &&
+    boardGameArray[5].classList.contains('markings-player-two')
+  ) {
+    playerTwoWins();
+  } else if (
+    // row 3
+    boardGameArray[6].classList.contains('markings-player-two') &&
+    boardGameArray[7].classList.contains('markings-player-two') &&
+    boardGameArray[8].classList.contains('markings-player-two')
+  ) {
+    playerTwoWins();
+  } else if (
+    //column 1
+    boardGameArray[0].classList.contains('markings-player-two') &&
+    boardGameArray[3].classList.contains('markings-player-two') &&
+    boardGameArray[6].classList.contains('markings-player-two')
+  ) {
+    playerTwoWins();
+  } else if (
+    //column 2)
+    boardGameArray[1].classList.contains('markings-player-two') &&
+    boardGameArray[4].classList.contains('markings-player-two') &&
+    boardGameArray[7].classList.contains('markings-player-two')
+  ) {
+    playerTwoWins();
+  } else if (
+    // column 3)
+    boardGameArray[2].classList.contains('markings-player-two') &&
+    boardGameArray[5].classList.contains('markings-player-two') &&
+    boardGameArray[8].classList.contains('markings-player-two')
+  ) {
+    playerTwoWins();
+  } else if (
+    //diagonal 1
+    boardGameArray[0].classList.contains('markings-player-two') &&
+    boardGameArray[4].classList.contains('markings-player-two') &&
+    boardGameArray[8].classList.contains('markings-player-two')
+  ) {
+    playerTwoWins();
+  } else if (
+    // diagonal 2
+    boardGameArray[2].classList.contains('markings-player-two') &&
+    boardGameArray[4].classList.contains('markings-player-two') &&
+    boardGameArray[6].classList.contains('markings-player-two')
+  ) {
+    playerTwoWins();
+  } else if (generalTurnCounter === 9) {
+    document.querySelector('.message-board').textContent =
+      'This is a draw. Pirates... parlay!';
+  } else {
+    console.log('keep on playing');
+  }
+}
+
+////////// PLAYERS INTERACTION ///////
+
+//Dialog insults function
+
+function playerOneDialogEachTurn() {
+  var randomInsult = Math.floor(Math.random() * playerOneInsultsArray.length);
+  document.querySelector('.dialogs-player-one').textContent =
+    playerOneInsultsArray[randomInsult];
+  speechBubblePlayerOneAppear();
+  speechBubblePlayerTwoDisappar();
+}
+
+function playerTwoDialogEachTurn() {
+  var randomInsult = Math.floor(Math.random() * playerTwoInsultsArray.length);
+  document.querySelector('.dialogs-player-two').textContent =
+    playerTwoInsultsArray[randomInsult];
+  speechBubblePlayerTwoAppear();
+  speechBubblePlayerOneDisappar();
+}
+
+// speech bubble dissapear
+function speechBubblePlayerOneDisappar() {
+  var speechBubbleToAppear = document.querySelector('#speech-player-one');
+  speechBubbleToAppear.classList.add('speech-disappear');
+}
+
+function speechBubblePlayerTwoDisappar() {
+  var speechBubbleToAppear = document.querySelector('#speech-player-two');
+  speechBubbleToAppear.classList.add('speech-disappear');
+}
+
+//function bubble appear
+function speechBubblePlayerOneAppear() {
+  var speechBubbleToAppear = document.querySelector('#speech-player-one');
+  speechBubbleToAppear.classList.remove('speech-disappear');
+}
+
+function speechBubblePlayerTwoAppear() {
+  var speechBubbleToAppear = document.querySelector('#speech-player-two');
+  speechBubbleToAppear.classList.remove('speech-disappear');
+}
